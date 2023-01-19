ab2d1e33a7706423956659c8a7698d434d9313a5
AndreaMinelli <minelliandrea94@gmail.com>
2023-01-19 15:53:50 +0100

    Aggiunta AppHero con banner

---
+++ src/components/AppHero.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+  name: "AppHero",
+  data() {
+    return {
+      image: "one-piece.webp",
+    };
+  },
+  computed: {
+    buildImageUrl() {
+      return new URL(`./img/${this.image}`, import.meta.url).href;
+    },
+  },
+};
+</script>
+
+<template>
+  <figure>
+    <img :src="this.buildImageUrl" alt="banner image" />
+  </figure>
+</template>
+
+<style scoped>
+figure {
+  text-align: center;
+}
+img {
+  max-width: 100%;
+}
+</style>
